e70da67ac3132263492b158fc3fa5e309a2a8e8b
daphnierea <johnmarkovalo@gmail.com>
2019-10-29 20:49:53 +0800

    Di pa tapos na Landing peyj

--- chootor/resources/views/tutor/dashboard.blade.php
+++ chootor/resources/views/tutor/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
 @section('content')
 <h1>TUTOR</h1>
-<img src="..." class="img-fluid" alt="Responsive image">
+<img src="..." class="w-100 p-3" alt="Responsive image">
 @endsection
--- chootor/resources/views/welcome.blade.php
+++ chootor/resources/views/welcome.blade.php
@@ -1,12 +1,87 @@
-@extends('layout.app')
+{{-- HERE IS THE MAIN NAVBAR --}}
 
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CHOO-TOR</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="/css/sticky-footer.css" rel="stylesheet">
+    <link rel="shortcut icon" href="../img/logo.png" type="image/png">
+
+    <style>
+        .full-width-div {
+            position: absolute;
+            width: 100%;
+            left: 0;
+        }
+
+        img .profile{
+            size: 5%;
+        }
+        
+        .title{
+            font-size: 5em;
+            font-weight: bold;
+            color: navy;
+            bottom: 50%; 
+            left: 10%;
+        }
+
+        .sub{
+            font-size: 2em;
+            color: white;
+            bottom: 48%; 
+            left: 10%;
+        }
+
+        .bottom-left {
+            position: absolute;                       
+        }
+    </style>
+</head>
+ 
+<body>
+
     <div class="container">
-        <div class="img">
-        <img src="/img/bannn.png" alt="">
+        <div class="full-width-div">
+            <div class="col-lg-12 col-xs-6">
+                <img src="/img/dashboard1.png" class="w-100  inline-block" alt="Responsive image">
+                <p class="bottom-left title">CHOOTOR </p> 
+                <p class="bottom-left sub"> Looking for a Tutor? We're here to help! </p>
+            </div>
+                
+            <div class="row">
+                <div class="col-lg-4" >
+                    <img src="/img/mahda.jpg" class="profile img-responsive rounded">
+                </div>
+                <div class="col-lg-4" >
+                    <img src="/img/mahda.jpg" class="profile img-responsive rounded">
+                </div>
+                <div class="col-lg-4" >
+                    <img src="/img/mahda.jpg" class="profile img-responsive rounded">
+                </div>
+            </div>
         </div>
     </div>
-    <h1>Company Name</h1>
+
     
-@endsection
+    <!-- Footer -->
+    <footer class="page-footer font-small fixed-bottom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">©
+        <a> Choochoo 2019</a>
+    </div>
+    <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+ 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
 
