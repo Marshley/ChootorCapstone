fb2620e5308708fbeaff0d3ecdb614e4303dbd38
Marshley <xmash16x@gmail.com>
2019-11-04 18:48:57 +0800

    tutor status notification

--- chootor/app/Http/Controllers/AdminController.php
+++ chootor/app/Http/Controllers/AdminController.php
@@ -9,11 +9,6 @@ use App\Location;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $subject = Subject::all();
@@ -24,23 +19,10 @@ class AdminController extends Controller
         $location = Location::all();
         return view('admin.location')->with('location', $location);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request, Subject $subject)
     {   
         // Create Subject
@@ -53,54 +35,25 @@ class AdminController extends Controller
         $location->create($request->toArray());
         return redirect('/location');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Request $request)
     {
         $users = User::all();
         return view('admin.dashboard')->with('users', $users);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, User $user)
     {
         // Update of tutor status
         $user->update($request->toArray());
 
-        auth()->user()->notify(new BookingRequestNotif());
+        $user->notify(new \App\Notifications\TutorStatusNotification('Tutor has been '.$user->status));
 
         return redirect('/admindashboard');
         
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
