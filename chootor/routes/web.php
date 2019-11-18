03b6310df600727c85ba1294b19e386f52dcd1e4
Marshley <xmash16x@gmail.com>
2019-11-18 18:43:40 +0800

    feedback

--- chootor/routes/web.php
+++ chootor/routes/web.php
@@ -26,8 +26,10 @@ Route::get('/logout', 'SessionController@destroy');
 Route::get('/tuteedashboard','TuteeController@index');
 Route::get('/tuteeprofile','TuteeController@tuteeprofile');
 Route::get('/booked','TuteeController@bookeddisplay');
+Route::get('/history','TuteeController@donesessiondisplay');
 Route::post('/booking','TuteeController@store');
 Route::post('/updatetuteeprofile','TuteeController@updateprofile');
+Route::post('/feedback/{booking}','TuteeController@feedback');
 // Tutor Routes
 Route::get('/tutorschedule','TutorController@create');
 Route::get('/request','TutorController@bookingrequest');
